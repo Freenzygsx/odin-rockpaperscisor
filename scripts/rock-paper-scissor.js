f48c00a0de46acdfcec721efa05522ee40c5dc8f
Freenzygsx <ezziel.trabajo@gmail.com>
2022-10-29 01:25:28 -0400

    Fix bug piedra choices

--- scripts/rock-paper-scissor.js
+++ scripts/rock-paper-scissor.js
@@ -15,23 +15,25 @@ let playerWins = 0; // Declaro las victorias del jugador
 let computerWins = 0; // Declaro las victorias de la computadora
 let draw = 0; // Declaro los empates
 
-for (i=0; i<=5; i++) { // Mientras que el numero de partidas sea menor a 5, jugamos
+for (i=0; i<5; i++) { // Mientras que el numero de partidas sea menor a 5, jugamos
+    // Seteamos las opciones por vuelta 
+    let computerChoice;
+    computerChoice = options[randomComputerChoice(options)]; // Hago que la computadora tenga su opcion
+    let playerFinalChoice;
+    playerFinalChoice = playerChoice();
+    
     // declaramos victorias y derrotas con piedra 
-    let computerChoice = options[randomComputerChoice(options)]; // Hago que la computadora tenga su opcion
-    let playerFinalChoice = playerChoice();
-
-    if ( playerFinalChoice == "piedra" && playerFinalChoice == "tijera") {
+    if ( playerFinalChoice == "piedra" && computerChoice == "tijera") {
         console.log("Jugador gana una");
         playerWins += 1;
     } else if (playerFinalChoice == "piedra" && computerChoice == "papel") {
         console.log("Computadora gana una");
         computerWins += 1;
-    } else if (playerFinalChoice == "piedra" && computerChoice == "papel") {
+    } else if (playerFinalChoice == "piedra" && computerChoice == "piedra") {
         console.log("Empate");
         draw += 1;
-    }
     // declaramos victorias y derrotas con papel
-    if (playerFinalChoice == "papel" && computerChoice == "piedra") {
+    } else if (playerFinalChoice == "papel" && computerChoice == "piedra") {
         console.log("Jugador gana una");
         playerWins +=1 ;
     } else if (playerFinalChoice == "papel" && computerChoice == "papel") {
@@ -40,16 +42,15 @@ for (i=0; i<=5; i++) { // Mientras que el numero de partidas sea menor a 5, juga
     } else if (playerFinalChoice == "papel" && computerChoice == "tijera") {
         console.log("Computadora gana una");
         computerWins += 1;
-    }
     //declaramos victorias y derrotas con tijera
-    if (playerFinalChoice == "tijera" && computerChoice == "piedra"){
+    } else if (playerFinalChoice == "tijera" && computerChoice == "piedra"){
         console.log("Computadora gana  una");
         computerWins += 1;
     } else if (playerFinalChoice == "tijera" && computerChoice == "papel") {
         console.log("Jugador gana una");
         playerWins += 1;
     } else if (playerFinalChoice == "tijera" && computerChoice == "tijera") {
-        console.log("empate");
+        console.log("Empate");
         draw += 1;
     }
 }
